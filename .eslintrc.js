e239acaabd37248df8d053982468627249529d94
NejcZdovc <nejc.zdovc@3zsistemi.si>
2017-09-13 09:58:51 +0200

    Adds brave specific rules on top of standard
    Auditors:
    
    Test Plan:

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "rules": {
+    // disallow assignment operators in conditional statements (no-cond-assign)
+    // https://eslint.org/docs/rules/no-cond-assign
+    "no-console": 2
+  }
+}
